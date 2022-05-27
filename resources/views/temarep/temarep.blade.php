f4b0e3051b17986c03a51080bceaf0fe399686be
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-27 11:05:29 -0300

    remanejamento dos blocos da pagina e tag required adicionada ao input

--- resources/views/temarep/temarep.blade.php
+++ resources/views/temarep/temarep.blade.php
@@ -10,11 +10,9 @@
     <form action="temarep" method="POST">
         @csrf
         <div class="input-group mb-3">
-            <input placeholder="Criar Tema..." type="text" class="form-control" id="nmTema" name="nmTema">
-            <input type="submit" class="btn btn-primary" value="Criar Tema" class="btn btn-outline-secondary"
-                id="button-addon2">
+            <input placeholder="Criar Tema..." type="text" class="form-control" id="nmTema" name="nmTema" aria-label="Criar Tema" aria-describedby="button-addon2" required />
+            <input type="submit" class="btn btn-primary" value="Criar Tema" class="btn btn-outline-secondary" id="button-addon2">
         </div>
-
     </form>
 </div>
 
@@ -43,8 +41,5 @@
     </table>
 
 </div>
-
-
-
 
 @endsection
