1c8c1f56762a05d81c886652e75c596b7eb81657
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-30 15:35:48 -0300

    inicio da querry de representacoes(incompleta)

--- app/Exports/RepresentacoesExport.php
+++ app/Exports/RepresentacoesExport.php
@@ -5,13 +5,31 @@ namespace App\Exports;
 use App\Models\Representacoe;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class RepresentacoesExport implements FromCollection
+use Illuminate\Contracts\View\view;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Database\Eloquent\Collection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class RepresentacoesExport implements FromView, ShouldAutoSize
 {
+    use Exportable;
     /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+     * @return \Illuminate\Support\Collection
+     */
+
+    public function view(): view
     {
-        return Representacoe::all();
+        //Falta terminar a querry
+        return view('exports.representacoes', [
+            'representacoes' => Representacoe::join('instancias', 'representacoes.cdInstancia', '=', 'instancias.cdInstancia')
+                ->leftjoin('representante_suplentes', 'representante_suplente.cdRepSup', '=', 'representacoes.cdTitular')
+
+
+
+        ]);
     }
 }
