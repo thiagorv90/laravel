12620662c3e3acd7fa0a40313e07386ac362f462
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-01 14:16:02 -0300

    refatoracao da blade instituicao

--- app/Exports/InstanciasExport.php
+++ app/Exports/InstanciasExport.php
@@ -12,15 +12,25 @@ use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use App\Invoice;
+use Maatwebsite\Excel\Concerns\Construct;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 
 
-class InstanciasExport implements FromView, ShouldAutoSize
+class InstanciasExport implements FromView, ShouldAutoSize, WithDrawings
 
 {
+    protected $id;
     use Exportable;
     /**
      * @return \Illuminate\Support\Collection
      */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
     public function view(): view
     {
         return view('exports.instancias', ['instancias' => Instancia::join('tema_representacoes', 'instancias.cdTema', '=', 'tema_representacoes.cdTema')
@@ -28,7 +38,19 @@ class InstanciasExport implements FromView, ShouldAutoSize
             ->leftjoin('representacoes', 'instancias.cdInstancia', '=', 'representacoes.cdInstancia')
             ->leftjoin('representante_suplentes', 'representacoes.cdTitular', '=', 'cdRepsup')
             ->leftjoin('contatos', 'contatos.cdInstancia', '=', 'instancias.cdInstancia')
-
+            ->where('instancias.cdInstancia', '=', $this->id)
             ->get()]);
+    }
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('Logo');
+        $drawing->setDescription('FIBRA');
+        $drawing->setPath(public_path('/image/fibralogo.png'));
+        $drawing->setHeight(90);
+        $drawing->setWidth(250);
+        $drawing->setCoordinates('A2');
+
+        return $drawing;
     }
 }
