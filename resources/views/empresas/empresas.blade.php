88a0a02cab4722f7eccc024e3ace126f0c11741c
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-26 13:25:21 -0300

    ajustes na pagina de empresa

--- resources/views/empresas/empresas.blade.php
+++ resources/views/empresas/empresas.blade.php
@@ -3,12 +3,10 @@
 @section('title', 'HDC Events')
 
 @section('content')
-    
 
     <h1>Empresas:</h1>
 
-
-    <div class="col-md-10 offset-md-1 dashboard-events-container">
+    <div class="container">
         <table class="table">
             <thead>
             <tr>
@@ -21,43 +19,39 @@
                 <tr>
                     <td scropt="row">{{$empresa->cdEmpresa}}</td>
                     <td><a>{{ $empresa->nmEmpresa }}</a></td>
-                    <td><a href="/empresas/edit/{{$empresa->cdEmpresa}}" class="btn btn-info edit-btn">
+                    <td>
+                        <a href="/empresas/edit/{{$empresa->cdEmpresa}}" class="btn btn-info edit-btn">
                             <ion-icon name="create-outline"></ion-icon>
                         </a>
+                    </td>
                 </tr>
             @endforeach
             </tbody>
         </table>
-    </div>
-    <br>
-    @if (!$empresas->isEmpty())
-    <form action="empresas/{{$empresa->cdEmpresa}}/search" method="GET">
-        @csrf
-        <div class="row">
-            <div class="col-lg-10">
-                <div class="form-group">
-                    <input type="text" class="form-control" value="" name="query" id="query" placeholder="busca">
-                    <button class="navbar-search__buttton">
-                        <i class="fa fa-search"></i>
-                    </button>
+        <br>
+        @if (!$empresas->isEmpty())
+            <form action="empresas/{{$empresa->cdEmpresa}}/search" method="GET">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" value="" name="query" id="query"
+                           placeholder="Buscar Empresa..."
+                           aria-label="Buscar Empresa" aria-describedby="button-addon2" required/>
+                    <input type="submit" class="btn btn-primary" value="Buscar" id="button-addon2">
                 </div>
-            </div>
-        </div>
-    </form>
-    @endif
-    <br>
+            </form>
+        @endif
+        <br>
 
-    <h1>Criar Empresas:</h1>
-    <div id="event-create-container" class="col-md-10 offset-md-1">
-        <form action="/empresas" method="POST">
-            @csrf
-            <div class="input-group mb-3">
-                <input placeholder="Criar Empresa..." type="text" class="form-control" id="nmEmpresa" name="nmEmpresa"
-                       aria-label="Criar Empresa" aria-describedby="button-addon2" required/>
-                <input type="submit" class="btn btn-primary" value="Criar Empresa" class="btn btn-outline-secondary"
-                       id="button-addon2">
-            </div>
-        </form>
+        <h1>Criar Empresas:</h1>
+            <form action="/empresas" method="POST">
+                @csrf
+                <div class="input-group mb-3">
+                    <input placeholder="Criar Empresa..." type="text" class="form-control" id="nmEmpresa"
+                           name="nmEmpresa"
+                           aria-label="Criar Empresa" aria-describedby="button-addon2" required/>
+                    <input type="submit" class="btn btn-primary" value="Criar Empresa" id="button-addon2">
+                </div>
+            </form>
     </div>
 
 @endsection
